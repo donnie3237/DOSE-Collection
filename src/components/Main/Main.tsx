cc9eefe78e5beb32edd66dab637bd5421560e4d5
SoRa <paradorn3237@gmail.com>
2022-12-10 20:49:04 +0700

    .

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './main.scss'
+const Ground = require('./img/Ground.png')
+const MarsMt = require('./img/mars-mt.png')
 
 type Props = {}
 
@@ -8,12 +10,20 @@ function Main({}: Props) {
     <div className='MainPage'>
       <div className="Head-main">
         <h3>Hello!!</h3>
+        <div className="sun"></div>
       </div>
-      <div className="div2"></div>
+      <div className="div2">
+        <img src={Ground}/>
+      </div>
       <div className="Head-main2">
         <h3>We are there!!</h3>
     </div>
-    <div className="div3"></div>
+    <div className="div3">
+      <img src={MarsMt}/>
+    </div>
+    <div className="MarsMiss">
+
+    </div>
     </div>
     
   )
