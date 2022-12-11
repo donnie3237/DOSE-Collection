b2fa7b5149c4a36b60d5009792c3ce356f43cfa8
SoRa <paradorn3237@gmail.com>
2022-12-11 22:23:41 +0700

    .

--- src/components/ProDucts/ProDucts.tsx
+++ src/components/ProDucts/ProDucts.tsx
@@ -12,7 +12,8 @@ const ProDucts = (props: Props) => {
             <div className="Prod-Title">
                 <h1>Hello</h1>
             </div>
-            <div className="dose-lib prodchild" id='dose-lib'>lib</div>
+            <div className="dose-lib prodchild" id='dose-lib'>lib
+            <a href="./MyProd/DosE-LIB Setup 1.0.0.exe" download>download</a></div>
             <div className="dose-ml prodchild" id='dose-ml'>ml</div>
             <div className="dose-crud prodchild" id='dose-crud'>crud</div>
             <div className="dose-iot prodchild" id='dose-iot'>iot</div>
